e01efab4ced9450db6485c1961cc5ce38fcbe8ae
YellowFlash2012 <cyclop9@hotmail.com>
2022-06-02 06:51:42 +0100

    created a bunch of templates, create, update, delete views. Created models and forms for adding new projects

---
+++ projects/forms.py
@@ -0,0 +1,8 @@
+from dataclasses import fields
+from django.forms import ModelForm
+from .models import Project
+
+class ProjectForm(ModelForm):
+    class Meta:
+        model = Project
+        fields = ["title", "description", "featured_img", "demo_link","source_link","tags"]
