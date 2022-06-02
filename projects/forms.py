53894c532cfa4261120a718101dc8ae13008223e
YellowFlash2012 <cyclop9@hotmail.com>
2022-06-02 08:41:48 +0100

    set static files and modified the look and feel of projects.html, navbar.html, single-project.html and create-project.html

--- projects/forms.py
+++ projects/forms.py
@@ -1,4 +1,5 @@
 from dataclasses import fields
+from django import forms
 from django.forms import ModelForm
 from .models import Project
 
@@ -6,3 +7,17 @@ class ProjectForm(ModelForm):
     class Meta:
         model = Project
         fields = ["title", "description", "featured_img", "demo_link","source_link","tags"]
+
+        widgets = {
+            "tags": forms.CheckboxSelectMultiple()
+        }
+
+    def __init__(self, *args, **kwargs):
+        super(ProjectForm, self).__init__(*args, **kwargs)
+
+        #1st way of modifying field classes
+        # self.fields['title'].widget.attrs.update({'class':'input'})
+
+        #2nd way of modifying field classes
+        for name, field in self.fields.items():
+            field.widget.attrs.update({'class':'input'})
