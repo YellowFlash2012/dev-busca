e9f59cb14759023b8c108a2d0535ce6288916505
YellowFlash2012 <cyclop9@hotmail.com>
2022-06-02 16:01:48 +0100

    created the users app, profile model, connected profile model to project model, created a few users in admin and rendered users' names on projects.html

--- projects/models.py
+++ projects/models.py
@@ -1,9 +1,11 @@
 from turtle import title
 from django.db import models
+from users.models import Profile
 import uuid
 
 # Create your models here.
 class Project(models.Model):
+    owner = models.ForeignKey(Profile, null=True, blank=True, on_delete=models.SET_NULL)
     title = models.CharField(max_length=200)
     description = models.TextField(null=True, blank=True)
     featured_img = models.ImageField(null=True, blank=True, default="default.jpg")
@@ -12,6 +14,7 @@ class Project(models.Model):
 
     tags = models.ManyToManyField("Tag", blank=True)
     vote_total = models.IntegerField(default=0, null=True, blank=True)
+    vote_ratio = models.IntegerField(default=0, null=True, blank=True)
     created_at = models.DateField(auto_now_add=True)
     id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
 
