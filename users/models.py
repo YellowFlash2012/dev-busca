a93f7679bb4425d45631d40b4c18ce914509e74e
YellowFlash2012 <cyclop9@hotmail.com>
2022-06-03 07:41:05 +0100

    configured signals

--- users/models.py
+++ users/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 import uuid
 
+
 # Create your models here.
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, null=True, blank=True)
@@ -33,3 +34,4 @@ class Skill(models.Model):
 
     def __str__(self):
         return str(self.name)
+
