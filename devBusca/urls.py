7bcbb070c9bf06042b288945221b5323af9ab0ea
YellowFlash2012 <cyclop9@hotmail.com>
2022-06-12 13:30:42 +0100

    static files were not showing in the finally deployed app on heroku so I did some modifications in project urls.py

--- devBusca/urls.py
+++ devBusca/urls.py
@@ -40,3 +40,4 @@ name="password_reset_done"),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
